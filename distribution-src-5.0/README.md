98c059ce4c21cdc14a7cf0c3d2d5725e042b9a12
Gabriel Ramos <gabriel.oliveira.ramos@gmail.com>
2019-10-27 22:45:29 -0300

    Added sources of distribution extension (version 5.0)

---
+++ distribution-src-5.0/README.md
@@ -0,0 +1,31 @@
+# distribution extension (version 5.0)
+
+This folder contains the source files of the `distribution` extension, used by all algorithms of the `pev_coalitions` project to compute the characteristic function of the coalitions. The present source files are compatible with NetLogo 5.3.1 (see not below).
+
+:::warning
+Important! The present instructions are compatible with NetLogo 5.3.1 (and, consequently, Java 8) and were tested on Mac OS X 10.15 and Ubuntu 18.04. Observe that this version of NetLogo is only compatible with Java 8, which is required to recompile the sources (see step 3 below). 
+:::
+
+## Instructions to recompile the sources and generate the jar file
+
+1. Open command line and access this directory.
+
+2. Copy NetLogo's jar files directory to the current directory. 
+    ```bash
+    cp -r <NetLogo_path>/Java/ ./NetLogoJars
+    ```
+    (Note: in NetLogo 5.3.1, the jar files are located in `NetLogo 5.3.1/Java`)
+
+3. Compile the source files.
+    ```bash
+    javac -classpath "NetLogoJars/NetLogo.jar" -d classes src/distribution/V2G.java src/distribution/Similarity.java src/distribution/DistributionManager.java
+    ```
+    (Note: the above command assumes that folder `classes` already exists)
+
+4. Create the jar file.
+    ```bash
+    jar cvfm distribution/distribution.jar manifest.txt -C classes .
+    ```
+    (Note: the above command assumes that folder `distribution` already exists)
+
+5. Now, in order to use the extension, you just need to copy the `distribution` folder to the same directory as the model that will use the extension.
---
+++ distribution-src-5.0/src/distribution/Similarity.java
@@ -0,0 +1,73 @@
+package distribution;
+
+import java.util.Random;
+
+import org.nlogo.api.*;
+
+public class Similarity extends DefaultReporter {
+
+	public Syntax getSyntax() {
+		return Syntax.reporterSyntax(new int[] {
+				Syntax.ListType(), 
+				Syntax.ListType(), 
+				Syntax.NumberType(), 
+				Syntax.NumberType(), 
+				Syntax.NumberType(), 
+				Syntax.NumberType(),
+				Syntax.BooleanType()
+			}, Syntax.ListType());
+	}
+	
+	public Object report(Argument args[], Context context) throws ExtensionException {
+		
+		//Parâmetros recebidos
+		LogoList C;          //elementos da coalizão (ordenado)
+		LogoList T;          //tempo de partida de cada elemento da coalizão (desordenado)
+		double epsilon=0.9;  //incentivo máximo dado pelo grid
+		double p=0.5;        //preço da unidade de energia
+		double c0=1000.0;    //constante normalizadora
+		int c1=12345;        //constante para definir potência do agente
+		boolean diffW=false; //se deve considerar potências diferentes para os veículos (true) ou se todos devem ter a mesma (false)
+		
+		//Parâmetros a serem calculados
+		double Tc=Double.MAX_VALUE; //duração da coalizão
+		double Wc=0.0;              //potência da coalizão
+		
+		//Lê os parâmetros
+		try {
+			C       = args[0].getList();
+			T       = args[1].getList();
+			epsilon = args[2].getDoubleValue();
+			p       = args[3].getDoubleValue();
+			c0      = args[4].getDoubleValue();
+			c1      = args[5].getIntValue();
+			diffW   = args[6].getBooleanValue();
+		}
+		catch(LogoException e) {
+			throw new ExtensionException( e.getMessage() ) ;
+		}
+		
+		//Se a coalizão é menor que 2, retorna 0 
+    	if (C.size() < 2) 
+    		return new Double(0);
+		
+    	//Calcula a duração da coalizão (menor tempo)
+    	for (int i = 0; i < T.size(); i++) {
+    		if ((Double)T.get(i) < Tc)
+    			Tc = (Double)T.get(i);
+    	}
+    	
+		//Calcula a potência da coalizão
+    	for (int i = 0; i < C.size(); i++) {
+    		int agente = (int)((Double)C.get(i)).doubleValue();
+    		Random r = new Random(agente*c1); //O número é sempre o mesmo para o agente e a constante em questão
+    		if (diffW)
+    			Wc += r.nextDouble() * 2 + 1; //De 1 a 3 kWh
+    		else
+    			Wc += 3.3;//todos iguais
+    	}
+		
+		return Math.min((Tc * Wc / c0) * epsilon, epsilon) * p;
+	}
+
+}
